6be0af43ce96de73f3434a72e63004bda1f0eb05
zrsmyof <zheltovrs@gmail.com>
2022-11-07 15:07:19 +0300

    Spring tutorial guess number game

---
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    // == fields ==
+    private final Random random = new Random();
+
+    @Getter
+    private final int maxNumber;
+    @Getter
+    private final int minNumber;
+
+    @Autowired
+    public NumberGeneratorImpl(@MaxNumber int maxNumber, @MinNumber int minNumber) {
+        this.maxNumber = maxNumber;
+        this.minNumber = minNumber;
+    }
+
+    // == public methods ==
+    @Override
+    public int next() {
+        return random.nextInt(maxNumber - minNumber) + minNumber;
+    }
+}
